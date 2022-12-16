7e378263c73d708ed68ed258433dc1db66e49532
Happyhobby11 <jeffreypang302@gmail.com>
2022-12-17 01:55:16 +0800

    restore playersService.spec.ts with minor update

--- server/spec/service/playersService.spec.ts
+++ server/spec/service/playersService.spec.ts
@@ -33,16 +33,18 @@ describe("Testing for playersService", () => {
 	let i: number = parseInt((Math.random() * accounts.length).toString());
 
 	beforeAll(async () => {
-		// await knex.schema.createTable(table.PLAYERS, (table) => {
-		// 	table.increments();
-		// 	table.string("name");
-		// 	table.string("email").notNullable();
-		// 	table.string("password").notNullable();
-		// 	table.string("image");
-		// 	table.integer("age");
-		// 	table.integer("gender");
-		// 	table.timestamps(true, true);
-		// });
+		if (!knex.schema.hasTable(table.PLAYERS)) {
+			await knex.schema.createTableIfNotExists(table.PLAYERS, (table) => {
+				table.increments();
+				table.string("name");
+				table.string("email").notNullable();
+				table.string("password").notNullable();
+				table.string("image");
+				table.integer("age");
+				table.integer("gender");
+				table.timestamps(true, true);
+			});
+		}
 		for (let j = 0; j < accounts.length; j++) {
 			finalAccounts[j].password = newPassword[j];
 		}
@@ -137,7 +139,7 @@ describe("Testing for playersService", () => {
 		);
 	});
 	afterAll(async () => {
-		await knex.schema.dropTable(table.PLAYERS);
+		await knex.table(table.PLAYERS).truncate();
 		await knex.destroy();
 	});
 });
