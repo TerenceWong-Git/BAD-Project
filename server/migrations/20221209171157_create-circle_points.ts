47bad5ad73e5e128dfcc1ba7f634fc29b68ead7e
Happyhobby11 <jeffreypang302@gmail.com>
2022-12-19 19:08:46 +0800

    added more seeds for ranking

--- server/migrations/20221209171157_create-circle_points.ts
+++ server/migrations/20221209171157_create-circle_points.ts
@@ -1,7 +1,8 @@
 import { Knex } from "knex";
+import { table } from "../utils/table";
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable("circle_points", (table) => {
+	await knex.schema.createTable(table.CIRCLE_POINTS, (table) => {
 		table.increments();
 		table.string("color").notNullable();
 		table.string("size").notNullable();
@@ -10,5 +11,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTable("circle_points");
+	await knex.schema.dropTable(table.CIRCLE_POINTS);
 }
--- server/seeds/create-game_mode.ts
+++ server/seeds/create-game_mode.ts
@@ -1,9 +1,10 @@
 import { Knex } from "knex";
+import { table } from "../utils/table";
 
 export async function seed(knex: Knex): Promise<void> {
 	// Deletes ALL existing entries
-	await knex("game_mode").del();
+	await knex(table.GAME_MODE).del();
 
 	// Inserts seed entries
-	await knex("game_mode").insert([{ name: "reaction" }, { name: "laser" }]);
+	await knex(table.GAME_MODE).insert([{ name: "reaction" }, { name: "laser" }]);
 }
