dba1541fdc565183d6542cea13101dacf12a2399
alext <alex.tam1927@gmail.com>
2022-12-19 19:15:29 +0800

    header component

--- server/private/js/modeSelection.js
+++ server/private/js/modeSelection.js
@@ -1,18 +1,5 @@
-onload();
 solo();
 duo();
-
-async function onload() {
-
-	document.querySelector("#logout").addEventListener("click", async (e) => {
-		e.preventDefault();
-		console.log("logout");
-		const res = await fetch("/players/logout", { method: "PUT" });
-		if (res.status === 200) {
-			window.location.href = `/`;
-		}
-	});
-};
 
 
 function solo() {
@@ -24,8 +11,9 @@ function solo() {
 		if (res.status === 200){
 			window.location.href = `/PreStart.html?matchId=matchId`
 			console.log('window location:', window.location.search);
-			
-			}
+			} else {
+				window.location.reload("/")
+			};
 		});
 };
 
