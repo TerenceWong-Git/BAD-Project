8082818680c165ce02ddb68e00f367b9f98f84e0
alext <alex.tam1927@gmail.com>
2022-12-21 01:57:39 +0800

    summary css, modeselection.js change prestart url

--- server/private/js/summary.js
+++ server/private/js/summary.js
@@ -5,7 +5,7 @@ loadResult();
 async function loadName() {
 	const resp = await fetch("/players/profile");
 	const infos = await resp.json();
-	let	 htmlStr = /*html*/ `<div>${infos.name}</div>`;
+	let	 htmlStr = /*html*/ `<div class="right">${infos.name}</div>`;
 	document.querySelector("#td1").innerHTML = htmlStr;
 };
 
@@ -13,10 +13,15 @@ async function loadResult() {
     const res = await fetch ("/players/my_ranking");
     const results = await res.json();
     console.log(results);
-    let HeighestPointHtmlStr = /*html*/ `<div>${results[0].points}</div>`;
-    document.querySelector("#td2").innerHTML = HeighestPointHtmlStr;
-    let CurrentPointHtmlStr = /*html*/ `<div></div>`;
-    document.querySelector("#td3").innerHTML = CurrentPointHtmlStr;
-}
+    let CurrentPointHtmlStr = /*html*/ `<div class="right"></div>`;
+    document.querySelector("#td2").innerHTML = CurrentPointHtmlStr;
+    let HighestPointHtmlStr = /*html*/ `<div class="right">${results[0].points}</div>`;
+    document.querySelector("#td3").innerHTML = HighestPointHtmlStr;
+};
+
+
+
+
+
 
 
