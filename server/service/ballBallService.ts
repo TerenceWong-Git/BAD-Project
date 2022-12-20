c07175ed51e10714f5772fda56c1bb86304a8a5d
unknown <haha@example.com>
2022-12-20 17:01:31 +0800

    trying ballBall MVC

--- server/service/ballBallService.ts
+++ server/service/ballBallService.ts
@@ -1,4 +1,5 @@
 import { Knex } from "knex";
+import { table } from "../utils/table";
 
 export class BallBallService {
 	constructor(private knex: Knex) {}
@@ -6,5 +7,16 @@ export class BallBallService {
 	async dummy() {
 		const result = await this.knex();
 		return result;
+	}
+	async providePoints(
+		players_id: number,
+		points: number,
+		matches_live_id: number
+	) {
+		const importResult = { players_id, points, matches_live_id };
+		const resultOfTheGame = await this.knex(table.MATCHES_RECORD).insert(
+			importResult
+		);
+		return resultOfTheGame;
 	}
 }
