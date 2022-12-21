e42e15cec3a8444f458341053764fc682475afeb
alext <alex.tam1927@gmail.com>
2022-12-22 04:15:27 +0800

    sweet alert

--- server/private/js/playerMainPage.js
+++ server/private/js/playerMainPage.js
@@ -1,7 +1,9 @@
+
 reaction();
 laser();
 profile();
 ranking();
+achi();
 
 function reaction() {
 	document.querySelector("#reaction").addEventListener("click", async (e) => {
@@ -13,7 +15,63 @@ function reaction() {
 function laser() {
 	document.querySelector("#laser").addEventListener("click", async (e) => {
 		e.preventDefault();
-		window.location.href = `/modeSelection.html?game=2`;
+		
+		Swal.fire({
+			title: 'Make a guess!',
+			text: "著跑鞋可以跑步，著行山鞋可爬山，著咩識飛?",
+			icon: 'info',
+			showCancelButton: true,
+			confirmButtonText: 'Duo mode!!!',
+			cancelButtonText: 'Answer!!!',
+			reverseButtons: true
+		  }).then((result) => {
+			if (result.isConfirmed) {
+			  Swal.fire(
+				'Thanks for your support!!!',
+				'We need your donation ($1000) to develop new function :)',
+				'error'
+			  )
+			} else if (
+			  result.dismiss === Swal.DismissReason.cancel
+			) {
+			  Swal.fire(
+				'著（雀）仔',
+				'We need your donation ($500) to develop new function :)',
+				'error'
+			  )
+			}
+		  })
+	});
+}
+function achi() {
+	document.querySelector("#achi").addEventListener("click", async (e) => {
+		e.preventDefault();
+		
+		Swal.fire({
+			title: 'Make a guess!',
+			text: "點解大肚婆唔可以食煙?",
+			icon: 'info',
+			showCancelButton: true,
+			confirmButtonText: 'Duo mode!!!',
+			cancelButtonText: 'Achievements!!!',
+			reverseButtons: true
+		  }).then((result) => {
+			if (result.isConfirmed) {
+			  Swal.fire(
+				'Thanks for your support!!!',
+				'We need your donation ($1000) to develop new function :)',
+				'error'
+			  )
+			} else if (
+			  result.dismiss === Swal.DismissReason.cancel
+			) {
+			  Swal.fire(
+				'因爲BB未夠18歲',
+				'We need your donation ($500) to develop new function :)',
+				'error'
+			  )
+			}
+		  })
 	});
 }
 function profile() {
