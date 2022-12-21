4d5d416430484bf4d18568dd93c069f090972c6f
alext <alex.tam1927@gmail.com>
2022-12-21 14:54:10 +0800

    ranking html

---
+++ server/private/js/ranking.js
@@ -0,0 +1,21 @@
+loadAllRank();
+// loadResult();
+
+
+async function loadAllRank() {
+	const resp = await fetch("/players/ranking");
+	const ranks = await resp.json();
+    console.log(ranks);
+	let	 htmlStr = ``;
+    for(const rank of ranks){
+        htmlStr += /*html*/ `
+        <tr>
+        <td>${rank.name}</td>
+        <td>${rank.points}
+        </tr>
+        `;
+
+    };
+    
+	document.querySelector("table").innerHTML = htmlStr;
+};
